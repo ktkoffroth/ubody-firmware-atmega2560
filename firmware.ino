4613e512335e6d3a470e3fc43cbd2c2667aaf350
ktkoffroth <ktkoffroth@gmail.com>
2020-04-26 23:09:48 -0400

    completed USART setup, transmit, and recieve functions

--- firmware.ino
+++ firmware.ino
@@ -1,5 +1,12 @@
+#define FOSC 16000000 // System Clock speed
+#define BAUD 9600 // Desired baud rate
+#define MYUBRR (FOSC/16/BAUD-1) // Actual baud rate
 
- int main(void)
+void USART_Init(unsigned int ubrr); // USART serial setup (half-duplex async mode)
+void USART_Transmit(unsigned char data); // Transmit a char in serial
+unsigned char USART_Recieve(void); // Recieve a char from serial
+
+int main(void)
    {
      //Initialise arduino ( so millis, delay work ).
      init();
@@ -7,7 +14,9 @@
 	USB.attach();
 #endif
 
-    // Setup code
+    // USART serial setup (Half-duplex async mode)
+    USART_Init(MYUBRR);
+    unsigned char echo;
 
      //Start loop
      while(true)
@@ -17,3 +26,31 @@
      }
      return 0;
    }
+
+void USART_Init(unsigned int ubrr)
+{
+  // Set baud rate
+  UBRR0H = (unsigned char) ubrr>>8;
+  UBRR0L = (unsigned char) ubrr;
+  // enable tx and rx
+  UCSR0B = (1<<RXEN0) | (1<<TXEN0);
+  // set frame format (1 stop bit, 8 data bits)
+  UCSR0C = 3<<UCSZ00;
+}
+
+void USART_Transmit(unsigned char data)
+{
+  // Wait for empty transmit buffer
+  while (!( UCSR0A & (1<<UDRE0)));
+
+  UDR0 = data;
+}
+
+unsigned char USART_Recieve(void)
+{
+  // Wait for recieve signal
+  while (!( UCSR0A & (1<<RXC0)));
+
+  // Get and return recieved data from buffer
+  return UDR0;
+}
